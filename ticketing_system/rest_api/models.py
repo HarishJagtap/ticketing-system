8a019f46396a139f051bb75f6cc17a9da0ef0767
Harish Jagtap <harish.jagtap23@gmail.com>
2021-05-12 20:44:05 +0530

    Added Ticket Model in Django Backend
    - Also included rest_api, rest_framwork app in Django Settings Installed Apps

--- ticketing_system/rest_api/models.py
+++ ticketing_system/rest_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+class Ticket(models.Model):
+
+    class Impact(models.IntegerChoices):
+        LOW = 1
+        MEDIUM = 2
+        HIGH = 3
+
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.TextField()
+    description = models.TextField(blank=True)
+    impact = models.PositiveSmallIntegerField(choices=Impact.choices)
+    category = models.TextField(blank=True)
+    assignee = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    group = models.ForeignKey(Group, on_delete=models.PROTECT, null=True)
